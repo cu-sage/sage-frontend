34292f10e49881a549c0077a42879b8f84adda2a
Ruicong Xie <rxie25@gmail.com>
2016-11-11 18:59:50 -0500

    added process chart

--- public/js/controllers/StudentTasksController.js
+++ public/js/controllers/StudentTasksController.js
@@ -1,7 +1,21 @@
 angular.module('mainApp')
+    /* .directive('progressChart', [ '$window', function ($window) {
+        return {
+            restrict: 'E',
+            template: '<div></div>',
+            scope: {
+                title: '@',
+                data: '='
+            },
+            link: function (scope, element) {
+                // angular.element(document.querySelector('.radial-progress').setAttribute('data-progress', '30'));
+            }
+        };
+    }]) */
     .controller('StudentTasksController', ['$scope', '$window', '$http', "$routeParams", function($scope, $window, $http, $routeParams) {
         console.log("testing");
         console.log($routeParams.sid);
+        $scope.progress = 60;
         $http.get("/stats/students/" + $routeParams.sid)
             .then(function(response) {
                 $scope.statuscode = response.status;

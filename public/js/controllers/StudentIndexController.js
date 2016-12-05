1efafba95c00646418411e182b9be4dc9ceaa181
Ruicong Xie <rxie25@gmail.com>
2016-12-04 22:57:08 -0500

    add upload

--- public/js/controllers/StudentIndexController.js
+++ public/js/controllers/StudentIndexController.js
@@ -9,13 +9,15 @@ angular.module('studentApp')
             return viewLocation === $location.path().split('/')[1];
         };
 
+        $scope.student_url = "https://s3.amazonaws.com/sage-student-avatar/"+$scope.sid+"-student.jpg";
+
+
+
+
         $http.get("/stats/students/" + $scope.sid)
             .then(function(response) {
                 $scope.statuscode = response.status;
                 $scope.statustext = response.statustext;
                 $scope.student = response.data;
-                //$scope.sid = sid;
-                //console.log(response.data);
-                //console.log($scope);
             });
     }]);

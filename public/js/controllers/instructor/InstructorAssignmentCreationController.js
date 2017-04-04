22e1baaa9bced1950292a78b1215875a7c5872ed
PlabanM1 <Plaban.Mohanty92@gmail.com>
2017-04-04 15:35:41 -0400

    Addition of Assignment creation features

--- public/js/controllers/instructor/InstructorAssignmentCreationController.js
+++ public/js/controllers/instructor/InstructorAssignmentCreationController.js
@@ -13,12 +13,32 @@ angular.module('instructorApp')
 
         $scope.design=function(){
             $scope.assignment={};
-            $scope.submitForm();
-            var i1id=$routeParams.sid;
-            var c1id=$routeParams.cid;
-            var path = "/coursePage/"+i1id+"/course/"+c1id+"/createAssignment/"+$scope.aid+"/design";
-            console.log(path);
-            $location.path(path);
+            newc={
+            order: $scope.assgn.order,
+            
+        };
+
+        console.log(newc);
+
+        $http({
+            method: 'POST',
+            url: "/stats/instructors/"+$routeParams.sid+"/course/"+$routeParams.cid+"/createAssignment/",
+            data: newc,
+            }).then(function(response) {
+                        //console.log(response.status);
+                        //console.log(response.data.message);
+                        $scope.aid=(response.data.message.testid);
+                        console.log($scope.aid);
+                        var i1id=$routeParams.sid;
+                        var c1id=$routeParams.cid;
+                        var path = "/coursePage/"+i1id+"/course/"+c1id+"/createAssignment/"+$scope.aid+"/design";
+                        console.log(path);
+                        $location.path(path);
+
+                                }
+                    
+            );
+                   
             //$scope.course.desc="";
         };
 

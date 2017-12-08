01e3c8ae294958a520b1cdcba58d3a08b7c9fcfe
Johan Sulaiman <lai.s.johan@gmail.com>
2017-12-07 16:22:16 -0800

    replacing static result with results GET request from SAGE-node

--- public/js/controllers/student/StudentAssessmentController.js
+++ public/js/controllers/student/StudentAssessmentController.js
@@ -1,60 +1,33 @@
 angular.module('studentApp')
-    .controller('StudentAssessmentController', function(Upload, $window, $location, $scope) {
+    .controller('StudentAssessmentController', function(Upload, $window, $location, $scope, $http, $log) {
         var path = $location.path().split('/');
         $scope.path = path[1];
         $scope.sid = path[path.length-1];
         $scope.assignmentID = path[path.indexOf('assessment')+1];
 
+        var results = null
         // poll a GET request to node, send every second
-        // save to result
-        var results = [
-                    {
-                        "description": "Block type sensing should be present",
-                        "pass": true,
-                        "actions": [
-                            {
-                            "type": 'action_say',
-                            "command": "good job"
-                            }
 
-                        ]
-                    },
-                    {
-                        "description": "Sprite 'Ball' should be present",
-                        "pass": false,
-                        "actions": [
-                            {
-                                "type": 'action_say',
-                                "command": "keep working on it"
-                            }
+        $log.info("Printing assessment " +$scope)
 
-                        ]
-                    },
-                    {
-                        "description": "Sprite 'Goal' should be present",
-                        "pass": true,
-                        "actions": [
-                            {
-                                "type": 'action_say',
-                                "command": "nicely done"
-                            }
+        $http({method: 'GET', url: 'http://localhost:8081/assess/game/123/objective/58d845736e4ddb3ce20ed1b3' })
+            .then(function(response) {
+                if (response.status == '403') {
+                    $log.info("Inside GET")
+                    $window.location.href = '/public/views/error.html';
+                } else {
 
-                        ]
-                    },
-                    {
-                        "description": "Sprite 'Goal' should be present",
-                        "pass": true,
-                        "actions": [
-                            {
-                                "type": 'action_say',
-                                "command": "nicely done"
-                            }
+                    fullResults = response.data;
+                    results = fullResults.assess.assessmentResult
+                    //$log.info(results)
 
-                        ]
-                    }
 
-                ];
-        $scope.results = results;
-        $scope.progress = "33";
-        $scope.level = "Basic";
+                    $scope.results = results;
+                    $scope.progress = "33";
+                    $scope.level = "Basic";
+                    $log.info($scope.results)
+                }
+            });
+
+
     });

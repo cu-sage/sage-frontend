28a539d2a1957f73f96d6e720d70460b0370aa9f
Johan Sulaiman <lai.s.johan@gmail.com>
2018-01-10 17:43:32 -0800

    bringing Objective Editor into Front-end

--- public/js/controllers/instructor/InstructorVAEController.js
+++ public/js/controllers/instructor/InstructorVAEController.js
@@ -14,13 +14,15 @@ angular.module('instructorApp')
         //     return hw_id == $routeParams.hid;
         // };
             //C:\Users\yuval\Documents\School\Fall2017\SAGELab\sage-editor\app\editor
-        document.getElementById("vae").innerHTML='<object type="text/html" data="http://localhost:3001/app/editor/" style="width:100%; height: 100%;"></object>'
+        //document.getElementById("vae").innerHTML='<object type="text/html" data="C:\\Users\\laisj\\Downloads\\SAGE\\sage-editor\\app\\editor\\index.html" style="width:100%; height: 100%;"></object>'
 
         $http.get("/stats/instructors/" + $routeParams.sid + "/courses/" + $routeParams.cid)
             .then(function(response) {
+                console.log("testing_VAE");
                 // $scope.statuscode = response.status;
                 // $scope.statustext = response.statustext;
                 $scope.course=response.data[0];
+                console.log($scope.course)
                
                 //$scope.assign = response.data[0].assignments;
                 //console.log($scope.assign);

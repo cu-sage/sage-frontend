d74c0f1294c34b33cf64cb44a7976e9e05505220
Harsimran Bath <harsimranb@outlook.com>
2018-03-02 13:12:58 -0500

    Additional notes/model updates from Jeff meeting

--- recommendationEngine2/models.js
+++ recommendationEngine2/models.js
@@ -1,9 +1,15 @@
+// Generate Mock Data using Generator
+
+// Context of the quest in which recommending games
+// ex: Struggling in ctConcept, recopmmend in focus of current quest (games)
 var gameModel = 
 [
     {
-        gameId: 2
-        difficulty: [1,2,3], // 1 = Easy, etc
-        ctConcepts: [1,2,3,4,5,6,] // 1 = Loops, etc.
+        gameId: 2,
+        difficulty: 1, // [1,2,3]; 1 = Easy, etc
+        gameFocus: null,// cirricula (context), cirricula analysis
+        ctConcepts: [1,2,3,4,5,6,] // 1 = Loops, etc., directly from hairball analysis
+        // ctConcepts/gameFocus: complication
     },
     // ...
 ]
@@ -13,8 +19,16 @@ var studentsModel =
     {
         userId: 1,
         gameId: 1,
+        difficulty: 2,
         duration: 500, // 500 seconds
-        ctScores: [3,3,2,1,2,0,] // scores per concept, order relation to ct concepts above
+        ctScoresMastery: mean([3,3,2,1,2,0,]),
+
+        // generated in scratch analyzer
+        pektScore: null, // Q: where stored?
+        pointScores: null, // Q: where stored? different meanings in parsons, etc.
+
+        objectPoints: null, // points earned when each statement completed, rubric points
+
     }
 ]
 
@@ -28,6 +42,7 @@ Algorithm:
         - ctScores
             1 (novice) to 3 (proficiency)
     Output:
+        Array of Games and Recommendation Scores
 
     Multi-Criteria:
         Games
@@ -35,9 +50,6 @@ Algorithm:
             Find similairty scores between difficulty og games
                 3-3: 100%, 3-2: 50%, 3-1: 0%
             
-    Uaer-Based: K-Means Culturing
+    User-Based: K-Means Culturing
         scores, games' scores, durations
-        
-
-
 */
